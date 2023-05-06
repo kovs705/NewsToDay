2507d2f6283c952c3c495edf9b6d1e0cc3680f80
kovs705 <smartkovsik@gmail.com>
2023-05-06 21:52:12 +0300

    Constants and UIhelper

---
+++ NewsToDay/Utilites/Resources/UIHelper.swift
@@ -0,0 +1,27 @@
+//
+//  UIHelper.swift
+//  NewsToDay
+//
+//  Created by Kovs on 06.05.2023.
+//
+
+import UIKit
+
+enum UIHelper {
+    
+    static func createTwoColumnLayout(in view: UIView) -> UICollectionViewLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 2
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: 35)
+        
+        // TODO: ----> подобрать высоту для ячеек !!!!! (itemSize height)
+        
+        return flowLayout
+    }
+}
