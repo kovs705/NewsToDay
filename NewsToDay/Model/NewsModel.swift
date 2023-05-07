acee66706cb183f8bd34d0923e09652987bb836a
kovs705 <smartkovsik@gmail.com>
2023-05-07 22:58:29 +0300

    gav

--- NewsToDay/Model/NewsModel.swift
+++ NewsToDay/Model/NewsModel.swift
@@ -20,24 +20,4 @@ struct News: Codable{
     let urlToImage:         String?
     let publishedAt:        String?
     let content:            String?
-    
-    init(
-        source:      Source,
-        author:      String?,
-        title:       String?,
-        description: String?,
-        url:         String?,
-        urlToImage:  String?,
-        publishedAt: String?,
-        content:     String?
-    ) {
-        self.source      = source
-        self.author      = author
-        self.title       = title
-        self.description = description
-        self.url         = url
-        self.urlToImage  = urlToImage
-        self.publishedAt = publishedAt
-        self.content     = content
-    }
 }
---
+++ NewsToDay/Model/User.swift
@@ -0,0 +1,10 @@
+//
+//  User.swift
+//  NewsToDay
+//
+//  Created by Kovs on 07.05.2023.
+//
+
+import Foundation
+
+
