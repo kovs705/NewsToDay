dc8baa41fb2add74c975b2b1430222dc1b224d7d
jstted <tedeev17@gmail.com>
2023-05-08 00:17:46 +0300

    create categories viewController file

---
+++ NewsToDay/Controllers/Categories/CategoriesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CategoriesViewController.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 07.05.2023.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
