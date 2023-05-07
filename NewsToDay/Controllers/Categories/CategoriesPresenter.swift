ab2ba6c4d77aafdb43187d5a8244202b1b3f8900
jstted <tedeev17@gmail.com>
2023-05-08 00:34:52 +0300

    create presenter

---
+++ NewsToDay/Controllers/Categories/CategoriesPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesPresenter.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 08.05.2023.
+//
+
+import Foundation
+
+protocol CategriesViewProtocol {
+    
+}
+
+protocol CategoriesPresenterProtocol {
+    
+}
+
+class CategoriesPresenter {
+    
+}
