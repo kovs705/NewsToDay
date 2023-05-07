64a41a5bb4723a9a7de3d2d7ae3cc7c324880fd8
jstted <tedeev17@gmail.com>
2023-05-08 00:41:17 +0300

    create coordinator

---
+++ NewsToDay/Support/Coordinator.swift
@@ -0,0 +1,16 @@
+//
+//  Coordinator.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 08.05.2023.
+//
+
+import UIKit
+
+protocol CoordinatorProtocol {
+    
+}
+
+class Coordinator: CoordinatorProtocol {
+    
+}
