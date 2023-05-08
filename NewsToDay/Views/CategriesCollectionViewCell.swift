1a4bbccb4978b96ad6dd4abcb98f9d09524b5906
jstted <tedeev17@gmail.com>
2023-05-08 19:32:45 +0300

    create collection cell

---
+++ NewsToDay/Views/CategriesCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategriesCollectionViewCell.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 08.05.2023.
+//
+
+import UIKit
+import SnapKit
+
+class CategriesCollectionViewCell: UICollectionViewCell {
+    let view = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Layout
+    
+    
+}
