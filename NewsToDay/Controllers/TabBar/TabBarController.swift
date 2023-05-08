315674ba3fa141aaa2bff0939c0a54d3d752b7f5
jstted <tedeev17@gmail.com>
2023-05-08 17:10:29 +0300

    add categories to tabBarController

--- NewsToDay/Controllers/TabBar/TabBarController.swift
+++ NewsToDay/Controllers/TabBar/TabBarController.swift
@@ -12,10 +12,26 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         UITabBar.appearance().tintColor = .systemBlue
-        viewControllers = [] // VCs here
-        
+        setupTabs()
     }
-    
+    private func setupTabs() {
+        let categories = Coordinator.getCategoriesModule()
+        
+        let navigationController = UINavigationController(rootViewController: categories)
+        
+        navigationController.tabBarItem = UITabBarItem(
+            title: "Categories",
+            image: UIImage(systemName: "square.grid.2x2"),
+            tag: 1
+        )
+        
+        let tabs = [navigationController] // VCs here
+        tabs.forEach { tab in
+            tab.navigationItem.largeTitleDisplayMode = .automatic
+            tab.navigationBar.prefersLargeTitles = true
+        }
+        setViewControllers(tabs, animated: true)
+    }
     
     
     
