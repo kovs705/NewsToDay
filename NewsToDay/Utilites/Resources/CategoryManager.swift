045e9096084c71a0d8eb5fbff47882ff4ee29df6
jstted <tedeev17@gmail.com>
2023-05-08 19:47:29 +0300

    update category manager

--- NewsToDay/Utilites/Resources/CategoryManager.swift
+++ NewsToDay/Utilites/Resources/CategoryManager.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 protocol CategoryManagerProtocol {
-    func getCategories() -> [Category]
+    var categories: [Category] { get }
+}
+
+class CategoryManager: CategoryManagerProtocol {
+    var categories: [Category] = [
+        Category(name: <#T##String#>, icon: <#T##String#>, apiName: <#T##String#>)
+    ]
 }
