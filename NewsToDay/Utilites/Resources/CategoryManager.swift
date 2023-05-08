4d9c419d4749e27b17a5b095c89e9732b3216538
jstted <tedeev17@gmail.com>
2023-05-08 19:31:20 +0300

    create categoryManager

---
+++ NewsToDay/Utilites/Resources/CategoryManager.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryManager.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 08.05.2023.
+//
+
+import Foundation
+
+protocol CategoryManagerProtocol {
+    func getCategories() -> [Category]
+}
