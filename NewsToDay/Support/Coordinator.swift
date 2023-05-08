e87c20a92a72329689732ffb781d02dfd57ca139
jstted <tedeev17@gmail.com>
2023-05-08 17:08:30 +0300

    add categories module to coordinator

--- NewsToDay/Support/Coordinator.swift
+++ NewsToDay/Support/Coordinator.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 protocol CoordinatorProtocol {
-    
+    static func getCategoriesModule() -> UIViewController
 }
 
 class Coordinator: CoordinatorProtocol {
-    
+    static func getCategoriesModule() -> UIViewController {
+        let view = CategoriesViewController()
+        let presenter = CategoriesPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
 }
