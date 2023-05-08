9bb54d97232e5aa02c0a4a417e806d72be315372
jstted <tedeev17@gmail.com>
2023-05-08 19:32:02 +0300

    set layouts

--- NewsToDay/Controllers/Categories/CategoriesViewController.swift
+++ NewsToDay/Controllers/Categories/CategoriesViewController.swift
@@ -16,20 +16,22 @@ final class CategoriesViewController: UIViewController {
     
     //MARK: - UI Elements
     
-    private var colletctionView = UICollectionView()
+    private var colletctionView: UICollectionView!
     private var discriptionLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        addSubViews()
+        setConstraints()
     }
     
     //MARK: - Setup UI
     
     private func setupUI() {
         view.backgroundColor = .systemBackground
+        self.title = "Categories"
         setupDiscriptionLabel()
+        setupCollectionView()
     }
     
     private func setupDiscriptionLabel() {
@@ -39,12 +41,21 @@ final class CategoriesViewController: UIViewController {
     }
     
     private func setupCollectionView() {
+        let layout = UIHelper.createTwoColumnLayout(in: view)
+        
+        colletctionView = UICollectionView(frame: .zero,
+                                           collectionViewLayout: layout)
         colletctionView.dataSource = self
         colletctionView.delegate = self
         colletctionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "categories")
     }
     
     //MARK: - Layout
+    
+    private func setConstraints() {
+        addSubViews()
+        makeConstraints()
+    }
     
     private func addSubViews() {
         let views: [UIView] = [colletctionView, discriptionLabel]
@@ -53,10 +64,15 @@ final class CategoriesViewController: UIViewController {
     
     private func makeConstraints() {
         colletctionView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(32)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+            make.bottom.equalTo(view)
         }
         discriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.bottom)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(8)
+            make.leading.equalTo(view).inset(16)
+            make.width.equalTo(view)
         }
     }
 }
@@ -71,11 +87,12 @@ extension CategoriesViewController: CategriesViewProtocol {
 
 extension CategoriesViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return 7
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "categories", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "categories",
+                                                      for: indexPath)
         cell.backgroundColor = .systemRed
         return cell
     }
