2575d478e6c30f9e0cafff14214f097805962a25
jstted <tedeev17@gmail.com>
2023-05-08 19:30:51 +0300

    create category model

---
+++ NewsToDay/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 08.05.2023.
+//
+
+import Foundation
+
+struct Category {
+    let name: String
+    let icon: String
+    let apiName: String
+}
