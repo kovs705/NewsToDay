8595d46a721550d19a1e6ae110caf46c984ca85f
Vladimir <motiw@icloud.com>
2023-05-09 22:01:49 +0600

    fix SectionHeader

--- NewsToDay/Controllers/Browse/SectionHeaderView.swift
+++ NewsToDay/Controllers/Browse/SectionHeaderView.swift
@@ -4,32 +4,34 @@ final class SectionHeaderView: UICollectionReusableView {
     static let id = "SectionHeaderView"
     let title = UILabel()
     let button = UIButton(type: .system)
-    let arrowImage = UIImage(named: "Arrow")
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        title.text = "Title"
-
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        button.setTitle("See all", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
-        button.semanticContentAttribute = .forceRightToLeft
-        button.setImage(arrowImage, for: .normal)
-
-        let stackView = UIStackView(arrangedSubviews: [title, button])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.spacing = 8
-
-        addSubview(stackView)
-        stackView.snp.makeConstraints { $0.edges.equalToSuperview()}
+        setupTitle()
+        setupButton()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
-    @objc private func didTapButton() {
+extension SectionHeaderView {
+    private func setupTitle() {
+        addSubview(title)
+        title.text = "Title"
+        title.snp.makeConstraints {
+            $0.top.left.bottom.equalToSuperview()
+        }
+        
+    }
     
+    private func setupButton() {
+        addSubview(button)
+        button.setTitle("See all", for: .normal)
+        button.snp.makeConstraints {
+            $0.centerY.equalTo(title)
+            $0.right.equalToSuperview()
+        }
     }
 }
