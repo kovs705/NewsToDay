b9a04135185e398f21481b4b4f89d79031ed5090
jstted <tedeev17@gmail.com>
2023-05-10 13:41:40 +0300

    set tabBar to root viewController

--- NewsToDay/Support/SceneDelegate.swift
+++ NewsToDay/Support/SceneDelegate.swift
@@ -14,10 +14,8 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = UINavigationController(rootViewController: AccountController())
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: TabBarController())
         window?.makeKeyAndVisible()
         
         configureNavigationBar()
