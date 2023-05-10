38362644dcef576490036529a1f5c4ccbbd4eb2c
kovs705 <smartkovsik@gmail.com>
2023-05-10 14:51:04 +0300

    deleted '?'

--- NewsToDay/Model/NewsModel.swift
+++ NewsToDay/Model/NewsModel.swift
@@ -16,7 +16,7 @@ struct News: Codable, Hashable {
     let source:             Source
     let author:             String?
     let title, description: String?
-    let url:                String?
+    let url:                String
     let urlToImage:         String?
     let publishedAt:        String?
     let content:            String?
