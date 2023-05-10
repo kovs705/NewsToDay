34128fd8250315bfd3e7a42d034455ff8a775774
kovs705 <smartkovsik@gmail.com>
2023-05-10 10:52:48 +0300

    Added everything form CategoriesModule

---
+++ NewsToDay/Views/Cells/CategoryCell/CategriesCollectionViewCell.swift .swift
@@ -0,0 +1,67 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 08.05.2023.
+//
+
+import UIKit
+import SnapKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - UI Elements
+    
+    var titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Public
+    
+    func setTitle(category: Category) {
+        titleLabel.text = "\(category.icon)  \(category.name.capitalized)"
+    }
+    
+    //MARK: - SetupUI
+    
+    private func setupViews() {
+        setupCell()
+        setupLabel()
+    }
+    
+    private func setupCell() {
+        backgroundColor = .clear
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 12
+        self.layer.borderColor = UIColor(named: Colors.greyLighter)?.cgColor
+    }
+    
+    private func setupLabel() {
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont(name: Fonts.semiBold, size: 16)
+        titleLabel.textColor = UIColor(named: Colors.greyDark)
+    }
+    
+    //MARK: - Layout
+    
+    private func setConstraints() {
+        contentView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(contentView)
+            make.centerY.equalTo(contentView)
+            make.height.equalTo(25)
+            make.leading.equalTo(contentView).inset(4)
+            make.trailing.equalTo(contentView).inset(4)
+        }
+    }
+    
+}
