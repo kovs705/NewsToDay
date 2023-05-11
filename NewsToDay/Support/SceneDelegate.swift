ca631a4b02e6392dbdec62f4d56dfe1d1d703b23
jstted <tedeev17@gmail.com>
2023-05-11 18:52:57 +0300

    update sceneDelegate file

--- NewsToDay/Support/SceneDelegate.swift
+++ NewsToDay/Support/SceneDelegate.swift
@@ -12,18 +12,11 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
         let coordinator = Coordinator()
-        window?.rootViewController = UINavigationController(rootViewController: TabBarController(coordinator: coordinator))
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = TabBarController(coordinator: coordinator)
         window?.makeKeyAndVisible()
-        
-        configureNavigationBar()
-    }
-    
-    func configureNavigationBar() {
-        UINavigationBar.appearance().tintColor = .systemBlue
     }
 }
 
