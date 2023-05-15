118b338d5d8de1c86347d5e4db23dabe5c6c66bb
jstted <tedeev17@gmail.com>
2023-05-15 17:42:54 +0300

    update categoryCell

--- NewsToDay/Views/Cells/CategoryCell/CategriesCollectionViewCell.swift .swift
+++ NewsToDay/Views/Cells/CategoryCell/CategriesCollectionViewCell.swift .swift
@@ -23,7 +23,6 @@ class CategoriesCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupViews()
         setConstraints()
     }
     
@@ -33,22 +32,29 @@ class CategoriesCollectionViewCell: UICollectionViewCell {
     
     //MARK: - Public
     
-    func setTitle(category: Category) {
+    func setTitle(category: Category, type: CategoriesCellType) {
         titleLabel.text = "\(category.icon)  \(category.name.capitalized)"
+        setupLabel()
+        switch type {
+        case .onboarding:
+            setupCellForOnboarding()
+        case .standard:
+            setupCellForStandart()
+        }
     }
     
     //MARK: - SetupUI
     
-    private func setupViews() {
-        setupCell()
-        setupLabel()
-    }
-    
-    private func setupCell() {
+    private func setupCellForStandart() {
         backgroundColor = .clear
         self.layer.borderWidth = 1
         self.layer.cornerRadius = 12
         self.layer.borderColor = UIColor(named: Colors.greyLighter)?.cgColor
+    }
+    
+    private func setupCellForOnboarding() {
+        backgroundColor = UIColor(named: Colors.greyLighter)
+        self.layer.cornerRadius = 12
     }
     
     private func setupLabel() {
@@ -71,4 +77,8 @@ class CategoriesCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    enum CategoriesCellType {
+        case onboarding
+        case standard
+    }
 }
