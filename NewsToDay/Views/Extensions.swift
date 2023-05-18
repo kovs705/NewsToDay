ae1ea5c0edef860f3b05955a13cf5bc5c70717c2
jstted <tedeev17@gmail.com>
2023-05-18 14:21:37 +0300

    add uiView extension

--- NewsToDay/Views/Extensions.swift
+++ NewsToDay/Views/Extensions.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
+//MARK: - UILabel
+
 extension UILabel {
     convenience init(textColor: Colors, textSize: CGFloat, font: Font, numberOfLines: Int) {
         self.init(frame: .zero)
         self.textColor = UIColor(named: textColor.rawValue)
         self.font = UIFont(name: "\(font.rawValue)", size: textSize)
         self.numberOfLines = numberOfLines
+    }
+}
+
+//MARK: - UIView
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
     }
 }
