aef2be82bfd49a222ee84c96c1f801f037709125
kovs705 <smartkovsik@gmail.com>
2023-05-18 09:29:48 +0300

    MVP for DetailVC, fixed crashing in ProfileVC, added InterLabel

--- NewsToDay/Support/Coordinator.swift
+++ NewsToDay/Support/Coordinator.swift
@@ -10,10 +10,15 @@ import UIKit
 protocol CoordinatorProtocol {
     func getCategoriesModule() -> UIViewController
     func getResultModule() -> UIViewController
-    func getCategoriesOnboardingModule() -> UIViewController
+    func getProfileModule() -> UIViewController
+    func getDetailVCModule(news: News) -> UIViewController
+    
+    func getAccountVCModule() -> UIViewController
+    func getRegisterVCModule() -> UIViewController
 }
 
 class Coordinator: CoordinatorProtocol {
+    
     func getCategoriesModule() -> UIViewController {
         let view = CategoriesViewController()
         let categories = CategoryManager()
@@ -30,11 +35,33 @@ class Coordinator: CoordinatorProtocol {
         return view
     }
     
-    func getCategoriesOnboardingModule() -> UIViewController {
-        let view = CategoriesOnboardingViewController()
-        let categories = CategoryManager()
-        let presenter = CategoriesOnboardingPresenter(view: view,
-                                                      categories: categories)
+    func getProfileModule() -> UIViewController {
+        let view = ProfileVC()
+        let presenter = ProfilePresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+    func getDetailVCModule(news: News) -> UIViewController {
+        let view = DetailVC()
+        let presenter = DetailPresenter(view: view, news: news)
+        
+        view.presenter = presenter
+        return view
+    }
+    
+    func getAccountVCModule() -> UIViewController {
+        let view = AccountController()
+        let presenter = AccountPresenter(view: view)
+        
+        view.presenter = presenter
+        return view
+    }
+    
+    func getRegisterVCModule() -> UIViewController {
+        let view = RegisterController()
+        let presenter = RegisterPresenter(view: view)
+        
         view.presenter = presenter
         return view
     }
