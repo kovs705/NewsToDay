82ca5582f2df0274efdc7c1df92dbc9097fe1f84
jstted <tedeev17@gmail.com>
2023-05-19 17:59:33 +0300

    create new network layer

---
+++ NewsToDay/Network/NewNetwork/ErrorResponse.swift
@@ -0,0 +1,26 @@
+//
+//  ErrorResponse.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 19.05.2023.
+//
+
+import Foundation
+
+enum ErrorResponse: String {
+    case apiError
+    case invalidEndpoint
+    case invalidResponse
+    case noData
+    case serializationError
+    
+    public var description: String {
+        switch self {
+        case .apiError: return "Ooops, there is something problem with the api"
+        case .invalidEndpoint: return "Ooops, there is something problem with the endpoint"
+        case .invalidResponse: return "Ooops, there is something problem with the response"
+        case .noData: return "Ooops, there is something problem with the data"
+        case .serializationError: return "Ooops, there is something problem with the serialization process"
+        }
+    }
+}
---
+++ NewsToDay/Network/NewNetwork/Request/TopHeadlinesRequest.swift
@@ -0,0 +1,44 @@
+//
+//  TopHeadlinesRequest.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 19.05.2023.
+//
+
+import Foundation
+
+struct TopHeadlinesRequest: DataRequest {
+    private let apiKey: String = "8e4cb38d557a45798ecdfa5b7c61f16e"
+        
+    var url: String {
+        let baseUrl = "https://newsapi.org/v2"
+        let path = "/top-headlines"
+        return baseUrl + path
+    }
+    
+    var headers: [String : String] {
+        [:]
+    }
+    
+    var queryItems: [String : String] {
+        [
+            "apiKey": apiKey
+        ]
+    }
+    
+    var method: HTTPMethod {
+        .get
+    }
+    
+    func decode(_ data: Data) throws -> [News]? {
+        let decoder = JSONDecoder()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
+        let response = try decoder.decode(NewsModel.self, from: data)
+        return response.articles
+    }
+    
+}
