3678c8c828e83491e19b90904f7a6171d34ceb42
jstted <tedeev17@gmail.com>
2023-05-20 01:14:04 +0300

    create onboarding module

---
+++ NewsToDay/Controllers/Onboarding/Onboarding/OnboardingPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingPresenter.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 20.05.2023.
+//
+
+import Foundation
---
+++ NewsToDay/Controllers/Onboarding/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingViewController.swift
+//  NewsToDay
+//
+//  Created by Mikhail Tedeev on 20.05.2023.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    private var nextButton = UIButton(type: .system)
+    private var picturesScrollView = UIScrollView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
